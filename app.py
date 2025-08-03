73f16d2c7ad1cdf21271d3460c0f918151821012
Promigo Ventures <promigoventures@gmail.com>
2025-08-03 10:06:18 +0100

    Updated app.py and created config.toml

--- app.py
+++ app.py
@@ -7,11 +7,115 @@ st.set_page_config(page_title='Sales Dashboard',
                    layout='wide')
 
 
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="supermarket_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales"
+    )
 
-df = pd.read_excel(
-    io='employee.xlsx',
-    engine="openpyxl",
-    sheet_name='Table1',
+#aDD "Hour" Column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+df = get_data_from_excel()
+
+st.sidebar.header('Please filter Here:')
+state = st.sidebar.multiselect(
+    "Select the State: ",
+    options = df["State"].unique(),
+    default = df["State"].unique()
 )
 
-st.dataframe(df)
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type: ",
+    options = df["Customer_type"].unique(),
+    default = df["Customer_type"].unique()
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender: ",
+    options = df["Gender"].unique(),
+    default = df["Gender"].unique()
+)
+
+df_selection = df.query(
+    "State == @state & Customer_type == @customer_type & Gender == @gender"
+)
+
+#----MAINPAGE-----
+st.title(":bar_chart: Sales_Dashboard")
+st.markdown("##")
+
+#TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction:,}")
+
+st.markdown("---")
+
+#SALES BY PRODUCT LINE (BAR CHART)
+sales_by_product_line = (
+    df_selection.groupby(by=["Product_line"]).sum()[["Total"]].sort_values(by="Total")
+)
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x = "Total",
+    y = sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#008388"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+
+fig_product_sales.update_layout(
+    plot_bgcolor = "rgba(0,0,0,0)",
+    xaxis= (dict(showgrid=False))
+)
+
+st.plotly_chart(fig_product_sales)
+
+#SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by hour</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True, key="product_sales_chart")
+right_column.plotly_chart(fig_product_sales, use_container_width=True, key="product_sales_chart_alt")
+
+
+#HIDE STREAMLIT STYLE
+hide_st_style = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+    </style>    
+"""
+
+st.markdown(hide_st_style, unsafe_allow_html=True)
